0182eda4122b02cb8dc25e325191a96f0c8d62c7
Wera Barinova <alisawera@gmail.com>
2025-02-11 22:11:09 +0300

    Создана таблица (автоматически)

--- 20250211_DOM/tbl.js
+++ 20250211_DOM/tbl.js
@@ -1,8 +1,35 @@
 function main_func() {
-    alert('Страница готова к работе');
+    console.log('Страница готова к работе');
+    add_table_to_page(create_table());
+}
+
+function add_table_to_page(tbl) {
+    // Функция добавляет кем-то созданную таблицу в тело страницы
+    document.body.appendChild(tbl);
+    // прикрепляем элемент к телу страницы
+}
+
+function create_table(columns, rows) {
+    // Создадим таблицу
+    let result = document.createElement('table');  // наш будущий результат
+    
+    // Создадим ряд (строку) таблицы TR - Tabnle Row
+    let tr = document.createElement('tr');
+
+    // прикрепим наш ряд (строку) к таблице
+    result.appendChild(tr);
+
+    // Создадим ячейку таблицы
+    let td = document.createElement('td');
+    
+    // Прикрепляем ячейку к строке
+    tr.appendChild(td);
+    // текст в ячейке
+    td.textContent = 'mew';
+    return result;  // вернули результат
 }
 
 window.addEventListener(
     'load',
-    main_func
+    main_func  // вызываем главную функцию при загрузке
 );
