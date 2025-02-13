ba85c696bc54ee9560152100dda0d98bc7fe2584
Wera Barinova <alisawera@gmail.com>
2025-02-13 20:26:30 +0300

    Добавлены координаты нажатия

--- 20250213_JS_mouse/code.js
+++ 20250213_JS_mouse/code.js
@@ -4,15 +4,24 @@ window.addEventListener(
 );
 
 function najal(kuda) {
+    console.log("Я работаю! нажал (куда)");
     console.log(kuda);
+}
+
+function pomeniai_tsvet(event) {
+    najal(kvadrat);
+    console.log('clientX = ', event.clientX);
+    console.log('pageX = ', event.pageX);
+    console.log('screenX = ', event.screenX);
+    console.log('clientY = ', event.clientY);
+    console.log('pageY = ', event.pageY);
+    console.log('screenY = ', event.screenY);
 }
 
 function main_function() {
     console.log('Страница готова');
     kvadrat.addEventListener(
         'click',
-        (event) => {
-            najal(kvadrat);
-        }
+        pomeniai_tsvet
     );
 }
