5d321450b0234c09d220bbea31a1df4d555e3cde
Wera Barinova <alisawera@gmail.com>
2025-02-13 21:29:07 +0300

    Работа с координатами и цветом

--- 20250213_JS_mouse/code.js
+++ 20250213_JS_mouse/code.js
@@ -18,10 +18,28 @@ function pomeniai_tsvet(event) {
     console.log('screenY = ', event.screenY);
 }
 
+function pokraska() {
+    // rgb(0, 0, 0) => rgb(255, 255, 255)
+    // rgb(0, 180, 0) => rgb(255, 0, 255)
+    // rgb(0, 0, 180) => rgb(0, 255, 255)
+    // rgb(200, 100, 0) => rgb(255, 255, 0)
+    let r = red_color.value;
+    let g = 20;
+    let b = 100;
+    console.log(`rgb(${r}, ${g}, ${b})`);
+    kvadrat.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    // Правило! Если в CSS свойство пишется через дефис,
+    // в JavaScript оно будет с больших букв (cameCase)
+}
+
 function main_function() {
     console.log('Страница готова');
     kvadrat.addEventListener(
         'click',
         pomeniai_tsvet
+    );
+    btn.addEventListener(
+        'click',
+        pokraska
     );
 }
