dbae358a70746e2a6bc96920a7f7fa6ffb507e46
Wera Barinova <alisawera@gmail.com>
2025-02-13 20:11:14 +0300

    Перехват загрузки страницы
    Теперь событие нажатия прикрепляется после загрузки страницы
    Мы получаем доступ к переменной (параметру) события

--- 20250213_JS_mouse/code.js
+++ 20250213_JS_mouse/code.js
@@ -1,3 +1,18 @@
+window.addEventListener(
+    'load',
+    main_function
+);
+
 function najal(kuda) {
     console.log(kuda);
+}
+
+function main_function() {
+    console.log('Страница готова');
+    kvadrat.addEventListener(
+        'click',
+        (event) => {
+            najal(kvadrat);
+        }
+    );
 }
