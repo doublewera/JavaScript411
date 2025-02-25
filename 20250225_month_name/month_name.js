c8e4f747653b4c4dc1392fec364a3f8989222423
Wera Barinova <alisawera@gmail.com>
2025-02-25 22:14:53 +0300

    Теперь задача решена с помощью циклов

--- 20250225_month_name/month_name.js
+++ 20250225_month_name/month_name.js
@@ -3,6 +3,7 @@
 // V Для каждого месяца указать русское название
 // Фон покрасить средствами JS+CSS
 
+
 function create_season_table() {
     // Вывести на страницу HTML таблицу
     // Элемент создается с названием в текстовом виде
@@ -11,66 +12,39 @@ function create_season_table() {
     // Создать элемент
     // Прикрепить элемент к телу страницы
     document.body.appendChild(tbl);
+    const seasons = ['winter', 'spring', 'summer', 'autumn'];
 
-    // Создали строку таблицы (tr) 
-    let tr = document.createElement('tr');
-    // Ссылка на таблицу уже была, мы к таблице прикрепили строку
-    tbl.appendChild(tr);
-    
-    // Создали переменную и первую ячейку
-    let td = document.createElement('td');
-    tr.appendChild(td);
-    //td.textContent = "Март Апрель май";
-    // делаем список!
-    // Создали элемент
-    let ol = document.createElement('ol');
-    ol.setAttribute("start", "3");
-    // Прикрепили элемент
-    td.appendChild(ol);
-    
-    // Создали элемент списка
-    let li = document.createElement('li');
-    li.textContent = "Март";
-    ol.appendChild(li);
-
-    // Создали ещё один элемент списка
-    li = document.createElement('li');
-    li.textContent = "Апрель";
-    ol.appendChild(li);
-    // 
-    li = document.createElement('li');
-    li.textContent = "Май";
-    ol.appendChild(li);
-
-    // переменная уже есть, создали вторую ячейку
-    td = document.createElement('td');
-    tr.appendChild(td);
-    td.textContent = "Лето";
-
-    tr = document.createElement('tr');
-    // Ссылка на таблицу уже была, мы к таблице прикрепили строку
-    tbl.appendChild(tr);
-    // Создали переменную и первую ячейку
-    td = document.createElement('td');
-    tr.appendChild(td);
-    td.textContent = "Осень";
-
-    // переменная уже есть, создали вторую ячейку
-    td = document.createElement('td');
-    tr.appendChild(td);
-    td.textContent = "Зима";
-    /*
-    <table>  // уже есть
-        <tr>  // уже есть
-            <td>Весна</td>
-            <td>Лето</td>
-        </tr>
-        <tr>
-            <td>Осень</td>
-            <td>Зима</td>
-        </tr>
-     </table>
-    */
+    let tr;
+    let td;
+    let ol;
+    let li;
+    for (let m = 2; m < 14; m++) {
+        // Перебираем месяцы, начиная с марта, потом поделим с остатком остаток ))
+        let month_number = (m + 1) % 12;  // 12, 13, 14 будут 0,1,2 - зима!
+        if (month_number % 3 == 0) {
+            // Если номер месяца делится нацело на три - начало сезона!
+            // Если на 6 - начало строки
+            if ((month_number + 3) % 6 == 0) {
+                // Создали строку таблицы (tr) 
+                tr = document.createElement('tr');
+                // Ссылка на таблицу уже была, мы к таблице прикрепили строку
+                tbl.appendChild(tr);
+            }
+            // Создали переменную и ячейку для сезона
+            td = document.createElement('td');
+            td.setAttribute('class', seasons[Math.round(month_number/3)]);
+            tr.appendChild(td);
+            // Создали список месяцев этого сезона
+            ol = document.createElement('ol');
+            ol.setAttribute("start", month_number);
+            // Прикрепили элемент
+            td.appendChild(ol);
+        }
+        // Создали элемент списка
+        li = document.createElement('li');
+        li.textContent = getWrittenMonth(month_number?month_number - 1:11);
+        ol.appendChild(li);
+    }
 }
 
 window.addEventListener(
@@ -79,27 +53,22 @@ window.addEventListener(
 );
 
 
-function getWrittenMonth(){
-    // Для каждого месяца указать русское название
-    const date = new Date();  // Текущая дата и время
-    console.log(date);
-    // date.getMonth() в JS возвращает месяцы С НУЛЯ
+function getWrittenMonth(month_number){
     const all_names = [  // Квадратные скобки - литерал списка
-        'января',   // 0
-        'февраля',  // 1
-        'марта',    // 2
-        'апреля',   // 3
+        'январь',   // 0
+        'февраль',  // 1
+        'март',     // 2
+        'апрель',   // 3
         'мая',      // 4
-        'июня',     // 5
-        'июля',     // 6
-        'августа',  // 
-        'сентября',
-        'октября',
-        'ноября',
-        'декабря'
+        'июнь',     // 5
+        'июль',     // 6
+        'август',   // 
+        'сентябрь',
+        'октябрь',
+        'ноябрь',
+        'декабрь'
     ];
     // return all_names[4];  // Вернули май
-    const month_number = date.getMonth();
     return all_names[month_number];
 }
   
