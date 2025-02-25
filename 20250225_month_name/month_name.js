acccfaf96423da10a17d6b0335044d73a4fd31cd
Wera Barinova <alisawera@gmail.com>
2025-02-25 21:02:56 +0300

    Оптимизировали js
    Получение названия месяца по индексу в массиве
    Функция создания таблицы, которая срабатывает по
    окончании загрузки страницы
    Таблица стилизована с помощью css, чтобы её было видно

--- 20250225_month_name/month_name.js
+++ 20250225_month_name/month_name.js
@@ -1,45 +1,45 @@
 // Задача: Вывести на страницу HTML таблицу
-// с 4-мы сезонами года. Для каждого месяца указать
-// русское название. Фон покрасить средствами JS+CSS
+// с 4-мы сезонами года.
+// V Для каждого месяца указать русское название
+// Фон покрасить средствами JS+CSS
+
+function create_season_table() {
+    // Вывести на страницу HTML таблицу
+    // Элемент создается с названием в текстовом виде
+    // надо созданный объект сохранить в переменную или константу
+    const tbl = document.createElement('table');
+    // Создать элемент
+    // Прикрепить элемент к телу страницы
+    document.body.appendChild(tbl);
+}
+
+window.addEventListener(
+    'load',  // Сделай так, браузер, чтобы после загрузки страницы
+    create_season_table  // вызывалась эта функция 
+);
+
 
 function getWrittenMonth(){
-    month = date.getMonth()+1
-
-    if (month == 1){
-        return 'января'
-    }
-    else if (month == 2){
-        return 'февраля'
-    }
-    else if (month == 3){
-        return 'марта'
-    }
-    else if (month == 4){
-        return 'апреля'
-    }
-    else if (month == 5){
-        return 'мая'
-    }
-    else if (month == 6){
-        return 'июня'
-    }
-    else if (month == 7){
-        return 'июля'
-    }
-    else if (month == 8){
-        return 'августа'
-    }
-    else if (month == 9){
-        return 'сентября'
-    }
-    else if (month == 10){
-        return 'октября'
-    }
-    else if (month == 11){
-        return 'ноября'
-    }
-    else if (month == 12){
-        return 'декабря'
-    }
+    // Для каждого месяца указать русское название
+    const date = new Date();  // Текущая дата и время
+    console.log(date);
+    // date.getMonth() в JS возвращает месяцы С НУЛЯ
+    const all_names = [  // Квадратные скобки - литерал списка
+        'января',   // 0
+        'февраля',  // 1
+        'марта',    // 2
+        'апреля',   // 3
+        'мая',      // 4
+        'июня',     // 5
+        'июля',     // 6
+        'августа',  // 
+        'сентября',
+        'октября',
+        'ноября',
+        'декабря'
+    ];
+    // return all_names[4];  // Вернули май
+    const month_number = date.getMonth();
+    return all_names[month_number];
 }
   
